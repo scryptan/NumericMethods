25ed98d05941e8ae90c797ef1cae8cca1ccad952
Maxim Tretyakov <scryptan@gmail.com>
2020-10-08 15:06:00 +0500

    init

---
+++ SecondMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecondMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
